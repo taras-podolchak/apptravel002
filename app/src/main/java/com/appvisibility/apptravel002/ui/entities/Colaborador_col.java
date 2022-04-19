33f6d2187c5a79ce95fb11ca4ff2f96208c3af3d
Quique <eboe62@hotmail.com>
2022-04-19 21:36:23 +0200

    220418: Septimo Commit rama Taras1 1.- Enlace ListView actividades con wikiloc 2.- Creación de C_05 para empezar a desarrollar la ficha de Valientes visible para los Colaboradores (provoca un error en V_03 que no he podido identificar)

---
+++ app/src/main/java/com/appvisibility/apptravel002/ui/entities/Colaborador_col.java
@@ -0,0 +1,227 @@
+package com.appvisibility.apptravel002.ui.entities;
+
+public class 	Colaborador_col	{
+
+	private	int	id_col	;
+	private	String	coche_col	;
+	private	String	apodo_col	;
+	private	String	contrasenna_col	;
+	private	String	actividadtipo_col	;
+	private	String	documentotipo_col	;
+	private	String	dni_col	;
+	private	String	nombre_col	;
+	private	String	apellido1_col	;
+	private	String	apellido2_col	;
+	private	String	numerocta_col	;
+	private	String	direccion_col	;
+	private	String	localidad_col	;
+	private	String	cpostal_col	;
+	private	String	pais_col	;
+	private	String	movil_col	;
+	private	String	email_col	;
+	private	String	usuariotipo_col	;
+	private	String	fotopropia_col	;
+	private	boolean	federado_col	;
+	private	String	federacionfoto_col	;
+	private	boolean	seguro_col	;
+	private	String	segurocompannia_col	;
+	private	String	seguropoliza_col	;
+	private	String	fechacaducidadseguro_col	;
+	private	String	fechaalta_col	;
+	private	int	antiguedad_col	;
+	private	String	fechabaja_col	;
+	private	boolean	solicitabaja_col	;
+	private	String	emergencia1telefono_col	;
+	private	String	emergencia1relacion_col	;
+	private	String	emergencia2telefono_col	;
+	private	String	emergencia2relacion_col	;
+	private	int	preferencia_col	;
+	private	int	fiabilidadpre_col	;
+	private int valoracionorgpre_col;
+																
+	public 	Colaborador_col	() {													
+		super();														
+	}															
+																
+	public 	Colaborador_col	(													
+			String	apodo_col	,											
+			String	dni_col	,											
+			String	nombre_col	,											
+			String	apellido1_col	,											
+			String	direccion_col	,											
+			String	localidad_col	,											
+			String	cpostal_col	,											
+			String	movil_col	,											
+			String	email_col	,											
+			boolean	federado_col												
+			) {													
+		super();														
+		this.	apodo_col=	apodo_col	;											
+		this.	dni_col=	dni_col	;											
+		this.	nombre_col=	nombre_col	;											
+		this.	apellido1_col=	apellido1_col	;											
+		this.	direccion_col=	direccion_col	;											
+		this.	localidad_col=	localidad_col	;											
+		this.	cpostal_col=	cpostal_col	;											
+		this.	movil_col=	movil_col	;											
+		this.	email_col=	email_col	;											
+		this.	federado_col=	federado_col	;											
+	}															
+
+	public 	Colaborador_col	(													
+			int	id_col	,											
+			String	coche_col	,											
+			String	apodo_col	,											
+			String	contrasenna_col	,											
+			String	actividadtipo_col	,											
+			String	documentotipo_col	,											
+			String	dni_col	,											
+			String	nombre_col	,											
+			String	apellido1_col	,											
+			String	apellido2_col	,											
+			String	numerocta_col	,											
+			String	direccion_col	,											
+			String	localidad_col	,											
+			String	cpostal_col	,											
+			String	pais_col	,											
+			String	movil_col	,											
+			String	email_col	,											
+			String	usuariotipo_col	,											
+			String	fotopropia_col	,											
+			boolean	federado_col	,											
+			String	federacionfoto_col	,											
+			boolean	seguro_col	,											
+			String	segurocompannia_col	,											
+			String	seguropoliza_col	,											
+			String	fechacaducidadseguro_col	,
+			String	fechaalta_col	,
+			int	antiguedad_col	,											
+			String	fechabaja_col	,
+			boolean	solicitabaja_col	,											
+			String	emergencia1telefono_col	,											
+			String	emergencia1relacion_col	,											
+			String	emergencia2telefono_col	,											
+			String	emergencia2relacion_col	,											
+			int	preferencia_col	,											
+			int	fiabilidadpre_col	,											
+			int	valoracionorgpre_col												
+			) {													
+		super();														
+		this.	id_col=	id_col	;											
+		this.	coche_col=	coche_col	;											
+		this.	apodo_col=	apodo_col	;											
+		this.	contrasenna_col=	contrasenna_col	;											
+		this.	actividadtipo_col=	actividadtipo_col	;											
+		this.	documentotipo_col=	documentotipo_col	;											
+		this.	dni_col=	dni_col	;											
+		this.	nombre_col=	nombre_col	;											
+		this.	apellido1_col=	apellido1_col	;											
+		this.	apellido2_col=	apellido2_col	;											
+		this.	numerocta_col=	numerocta_col	;											
+		this.	direccion_col=	direccion_col	;											
+		this.	localidad_col=	localidad_col	;											
+		this.	cpostal_col=	cpostal_col	;											
+		this.	pais_col=	pais_col	;											
+		this.	movil_col=	movil_col	;											
+		this.	email_col=	email_col	;											
+		this.	usuariotipo_col=	usuariotipo_col	;											
+		this.	fotopropia_col=	fotopropia_col	;											
+		this.	federado_col=	federado_col	;											
+		this.	federacionfoto_col=	federacionfoto_col	;											
+		this.	seguro_col=	seguro_col	;											
+		this.	segurocompannia_col=	segurocompannia_col	;											
+		this.	seguropoliza_col=	seguropoliza_col	;											
+		this.	fechacaducidadseguro_col=	fechacaducidadseguro_col	;											
+		this.	fechaalta_col=	fechaalta_col	;											
+		this.	antiguedad_col=	antiguedad_col	;											
+		this.	fechabaja_col=	fechabaja_col	;											
+		this.	solicitabaja_col=	solicitabaja_col	;											
+		this.	emergencia1telefono_col=	emergencia1telefono_col	;											
+		this.	emergencia1relacion_col=	emergencia1relacion_col	;											
+		this.	emergencia2telefono_col=	emergencia2telefono_col	;											
+		this.	emergencia2relacion_col=	emergencia2relacion_col	;											
+		this.	preferencia_col=	preferencia_col	;											
+		this.	fiabilidadpre_col=	fiabilidadpre_col	;											
+		this.	valoracionorgpre_col=	valoracionorgpre_col ;												
+	}															
+																
+	public 	int	getId_col	() {return 	id_col	;}	public void 	setId_col	(	int	id_col	) {this.	id_col=	id_col	;}	
+	public 	String	getCoche_col	() {return 	coche_col	;}	public void 	setCoche_col	(	String	coche_col	) {this.	coche_col=	coche_col	;}	
+	public 	String	getApodo_col	() {return 	apodo_col	;}	public void 	setApodo_col	(	String	apodo_col	) {this.	apodo_col=	apodo_col	;}	
+	public 	String	getContrasenna_col	() {return 	contrasenna_col	;}	public void 	setContrasenna_col	(	String	contrasenna_col	) {this.	contrasenna_col=	contrasenna_col	;}	
+	public 	String	getActividadtipo_col	() {return 	actividadtipo_col	;}	public void 	setActividadtipo_col	(	String	actividadtipo_col	) {this.	actividadtipo_col=	actividadtipo_col	;}	
+	public 	String	getDocumentotipo_col	() {return 	documentotipo_col	;}	public void 	setDocumentotipo_col	(	String	documentotipo_col	) {this.	documentotipo_col=	documentotipo_col	;}	
+	public 	String	getDni_col	() {return 	dni_col	;}	public void 	setDni_col	(	String	dni_col	) {this.	dni_col=	dni_col	;}	
+	public 	String	getNombre_col	() {return 	nombre_col	;}	public void 	setNombre_col	(	String	nombre_col	) {this.	nombre_col=	nombre_col	;}	
+	public 	String	getApellido1_col	() {return 	apellido1_col	;}	public void 	setApellido1_col	(	String	apellido1_col	) {this.	apellido1_col=	apellido1_col	;}	
+	public 	String	getApellido2_col	() {return 	apellido2_col	;}	public void 	setApellido2_col	(	String	apellido2_col	) {this.	apellido2_col=	apellido2_col	;}	
+	public 	String	getNumerocta_col	() {return 	numerocta_col	;}	public void 	setNumerocta_col	(	String	numerocta_col	) {this.	numerocta_col=	numerocta_col	;}	
+	public 	String	getDireccion_col	() {return 	direccion_col	;}	public void 	setDireccion_col	(	String	direccion_col	) {this.	direccion_col=	direccion_col	;}	
+	public 	String	getLocalidad_col	() {return 	localidad_col	;}	public void 	setLocalidad_col	(	String	localidad_col	) {this.	localidad_col=	localidad_col	;}	
+	public 	String	getCpostal_col	() {return 	cpostal_col	;}	public void 	setCpostal_col	(	String	cpostal_col	) {this.	cpostal_col=	cpostal_col	;}	
+	public 	String	getPais_col	() {return 	pais_col	;}	public void 	setPais_col	(	String	pais_col	) {this.	pais_col=	pais_col	;}	
+	public 	String	getMovil_col	() {return 	movil_col	;}	public void 	setMovil_col	(	String	movil_col	) {this.	movil_col=	movil_col	;}	
+	public 	String	getEmail_col	() {return 	email_col	;}	public void 	setEmail_col	(	String	email_col	) {this.	email_col=	email_col	;}	
+	public 	String	getUsuariotipo_col	() {return 	usuariotipo_col	;}	public void 	setUsuariotipo_col	(	String	usuariotipo_col	) {this.	usuariotipo_col=	usuariotipo_col	;}	
+	public 	String	getFotopropia_col	() {return 	fotopropia_col	;}	public void 	setFotopropia_col	(	String	fotopropia_col	) {this.	fotopropia_col=	fotopropia_col	;}	
+	public 	boolean	getFederado_col	() {return 	federado_col	;}	public void 	setFederado_col	(	boolean	federado_col	) {this.	federado_col=	federado_col	;}	
+	public 	String	getFederacionfoto_col	() {return 	federacionfoto_col	;}	public void 	setFederacionfoto_col	(	String	federacionfoto_col	) {this.	federacionfoto_col=	federacionfoto_col	;}	
+	public 	boolean	getSeguro_col	() {return 	seguro_col	;}	public void 	setSeguro_col	(	boolean	seguro_col	) {this.	seguro_col=	seguro_col	;}	
+	public 	String	getSegurocompannia_col	() {return 	segurocompannia_col	;}	public void 	setSegurocompannia_col	(	String	segurocompannia_col	) {this.	segurocompannia_col=	segurocompannia_col	;}	
+	public 	String	getSeguropoliza_col	() {return 	seguropoliza_col	;}	public void 	setSeguropoliza_col	(	String	seguropoliza_col	) {this.	seguropoliza_col=	seguropoliza_col	;}	
+	public 	String	getFechacaducidadseguro_col	() {return 	fechacaducidadseguro_col	;}	public void 	setFechacaducidadseguro_col	(	String	fechacaducidadseguro_col	) {this.	fechacaducidadseguro_col=	fechacaducidadseguro_col	;}
+	public 	String	getFechaalta_col	() {return 	fechaalta_col	;}	public void 	setFechaalta_col	(	String	fechaalta_col	) {this.	fechaalta_col=	fechaalta_col	;}
+	public 	int	getAntiguedad_col	() {return 	antiguedad_col	;}	public void 	setAntiguedad_col	(	int	antiguedad_col	) {this.	antiguedad_col=	antiguedad_col	;}	
+	public 	String	getFechabaja_col	() {return 	fechabaja_col	;}	public void 	setFechabaja_col	(	String	fechabaja_col	) {this.	fechabaja_col=	fechabaja_col	;}
+	public 	boolean	getSolicitabaja_col	() {return 	solicitabaja_col	;}	public void 	setSolicitabaja_col	(	boolean	solicitabaja_col	) {this.	solicitabaja_col=	solicitabaja_col	;}	
+	public 	String	getEmergencia1Telefono_col	() {return 	emergencia1telefono_col	;}	public void 	setEmergencia1Telefono_col	(	String	emergencia1telefono_col	) {this.	emergencia1telefono_col=	emergencia1telefono_col	;}	
+	public 	String	getEmergencia1Relacion_col	() {return 	emergencia1relacion_col	;}	public void 	setEmergencia1Relacion_col	(	String	emergencia1relacion_col	) {this.	emergencia1relacion_col=	emergencia1relacion_col	;}	
+	public 	String	getEmergencia2Telefono_col	() {return 	emergencia2telefono_col	;}	public void 	setEmergencia2Telefono_col	(	String	emergencia2telefono_col	) {this.	emergencia2telefono_col=	emergencia2telefono_col	;}	
+	public 	String	getEmergencia2Relacion_col	() {return 	emergencia2relacion_col	;}	public void 	setEmergencia2Relacion_col	(	String	emergencia2relacion_col	) {this.	emergencia2relacion_col=	emergencia2relacion_col	;}	
+	public 	int	getPreferencia_col	() {return 	preferencia_col	;}	public void 	setPreferencia_col	(	int	preferencia_col	) {this.	preferencia_col=	preferencia_col	;}	
+	public 	int	getFiabilidadpre_col	() {return 	fiabilidadpre_col	;}	public void 	setFiabilidadpre_col	(	int	fiabilidadpre_col	) {this.	fiabilidadpre_col=	fiabilidadpre_col	;}	
+	public 	int	getValoracionorgpre_col	() {return 	valoracionorgpre_col	;}	public void 	setValoracionorgpre_col	(	int	valoracionorgpre_col	) {this.	valoracionorgpre_col=	valoracionorgpre_col	;}	
+																
+	@Override															
+	public String toString() {															
+		StringBuilder builder = new StringBuilder();														
+		builder.append("	Colaborador_col	 [	id_col=	");	builder.append(	id_col+	" - ");							
+		builder.append(", 			coche_col=	");	builder.append(	coche_col+	" - ");							
+		builder.append(", 			apodo_col=	");	builder.append(	apodo_col+	" - ");							
+		builder.append(", 			contrasenna_col=	");	builder.append(	contrasenna_col+	" - ");							
+		builder.append(", 			actividadtipo_col=	");	builder.append(	actividadtipo_col+	" - ");							
+		builder.append(", 			documentotipo_col=	");	builder.append(	documentotipo_col+	" - ");							
+		builder.append(", 			dni_col=	");	builder.append(	dni_col+	" - ");							
+		builder.append(", 			nombre_col=	");	builder.append(	nombre_col+	" - ");							
+		builder.append(", 			apellido1_col=	");	builder.append(	apellido1_col+	" - ");							
+		builder.append(", 			apellido2_col=	");	builder.append(	apellido2_col+	" - ");							
+		builder.append(", 			numerocta_col=	");	builder.append(	numerocta_col+	" - ");							
+		builder.append(", 			direccion_col=	");	builder.append(	direccion_col+	" - ");							
+		builder.append(", 			localidad_col=	");	builder.append(	localidad_col+	" - ");							
+		builder.append(", 			cpostal_col=	");	builder.append(	cpostal_col+	" - ");							
+		builder.append(", 			pais_col=	");	builder.append(	pais_col+	" - ");							
+		builder.append(", 			movil_col=	");	builder.append(	movil_col+	" - ");							
+		builder.append(", 			email_col=	");	builder.append(	email_col+	" - ");							
+		builder.append(", 			usuariotipo_col=	");	builder.append(	usuariotipo_col+	" - ");							
+		builder.append(", 			fotopropia_col=	");	builder.append(	fotopropia_col+	" - ");							
+		builder.append(", 			federado_col=	");	builder.append(	federado_col+	" - ");							
+		builder.append(", 			federacionfoto_col=	");	builder.append(	federacionfoto_col+	" - ");							
+		builder.append(", 			seguro_col=	");	builder.append(	seguro_col+	" - ");							
+		builder.append(", 			segurocompannia_col=	");	builder.append(	segurocompannia_col+	" - ");							
+		builder.append(", 			seguropoliza_col=	");	builder.append(	seguropoliza_col+	" - ");							
+		builder.append(", 			fechacaducidadseguro_col=	");	builder.append(	fechacaducidadseguro_col+	" - ");							
+		builder.append(", 			fechaalta_col=	");	builder.append(	fechaalta_col+	" - ");							
+		builder.append(", 			antiguedad_col=	");	builder.append(	antiguedad_col+	" - ");							
+		builder.append(", 			fechabaja_col=	");	builder.append(	fechabaja_col+	" - ");							
+		builder.append(", 			solicitabaja_col=	");	builder.append(	solicitabaja_col+	" - ");							
+		builder.append(", 			emergencia1telefono_col=	");	builder.append(	emergencia1telefono_col+	" - ");							
+		builder.append(", 			emergencia1relacion_col=	");	builder.append(	emergencia1relacion_col+	" - ");							
+		builder.append(", 			emergencia2telefono_col=	");	builder.append(	emergencia2telefono_col+	" - ");							
+		builder.append(", 			emergencia2relacion_col=	");	builder.append(	emergencia2relacion_col+	" - ");							
+		builder.append(", 			preferencia_col=	");	builder.append(	preferencia_col+	" - ");							
+		builder.append(", 			fiabilidadpre_col=	");	builder.append(	fiabilidadpre_col+	" - ");							
+		builder.append(", 			valoracionorgpre_col=	");	builder.append(	valoracionorgpre_col+	" - ");							
+		builder.append("]");														
+		return builder.toString();														
+	}															
+}
